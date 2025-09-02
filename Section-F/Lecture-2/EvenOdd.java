63f29cba263c96b01a0f929e1ebd9d451a68e346
AjayCodingblock <ajaykumarkk017@gmail.com>
2025-09-02 19:45:17 +0530

    today.....

---
+++ Section-F/Lecture-2/EvenOdd.java
@@ -0,0 +1,13 @@
+// Program 2: Check if a number is Even or Odd
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 17;
+
+        // Check remainder
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
