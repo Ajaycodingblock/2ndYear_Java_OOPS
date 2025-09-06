8a82a890cb4e6b27e0a457b5c691523022c16be1
AjayCodingblock <ajaykumarkk017@gmail.com>
2025-09-06 14:25:12 +0530

    code

---
+++ Section-H/Lecture-12/StringIntro.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StringIntro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i =0 ;i<3;i++)
+        {
+        String str = sc.nextLine();
+    
+        
+            
+        }
+    }
+    
+}
---
+++ Section-H/Lecture-12/StringLength.java
@@ -0,0 +1,6 @@
+class StringLength {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("Length: " + str.length());
+    }
+}
