cb40bed5b0999fc9e14178d67c894e3198ee53e4
AjayCodingblock <ajaykumarkk017@gmail.com>
2025-11-23 09:54:43 +0530

    Collection Framework

--- Section-G/Lecture-25/CollectionFramework.java
+++ Section-G/Lecture-25/CollectionFramework.java
@@ -1,27 +1,32 @@
 import java.util.*;
 
+
 public class CollectionFramework {
     public static void main(String[] args) {
-        ArrayList <Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-          list.add(3);
-         list.add(4);
-        //   list.add(5);
-        // list.add(6);
+        ArrayList <Integer>  list = new ArrayList<>();
+        Scanner sc =  new Scanner(System.in);
+
+        list.add(45);
+        list.add(46);
+        list.add(47);
+        list.add(48);
+
+        System.out.println(list);
+        int n  = 5;
+
+
+        for(int i = 0;i<n;i++)
+        {
+             int x = sc.nextInt();
+             list.add(x);
+        }
+        System.out.println("Print using loop  : - ");
+int len = list.size();
+        for(int i = 0 ;i<len;i++)
+       {
+        System.out.println(list.get(i));
+       }
         
-System.out.println(list);
-list.remove(2);
-System.out.println("Print using Loop : - ");
-int n = list.size();
-
-
-for(int i  = 0;i<n;i++)
-{
-    System.out.println(list.get(i));
-}
-
-
     }
     
 }
---
+++ Section-H/Lecture-32/CollectionFrameWork.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class CollectionFrameWork {
+    public static void main(String[] args) {
+        ArrayList <Integer> list  = new ArrayList<>();
+        list.add(23);
+        list.add(45);
+        list.add(47);
+        list.add(67);
+        list.add(34);
+         list.remove(2);
+         System.out.println(list);
+         System.out.println(list.get(3));
+
+    }
+    
+}
